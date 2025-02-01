db26d029e44a0b41296f86dce5c342332adb3c6b
softhouse02 <softhouse20345@gmail.com>
2025-02-01 16:09:46 -0300

    atualização de título e criação de ícones

--- data/index.ts
+++ data/index.ts
@@ -1,14 +1,14 @@
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Projects", link: "#projects" },
+  { name: "Sobre", link: "#about" },
+  { name: "Projetos", link: "#projects" },
   { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+  { name: "Contato", link: "#contact" },
 ];
 
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "Promovemos a maior integração possivel para sua empresa",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
@@ -18,7 +18,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "I'm very flexible with time zone communications",
+    title: "Atendemos em qualquer lugar do mundo",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -28,8 +28,8 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "My tech stack",
-    description: "I constantly try to improve",
+    title: "Nossos Seviços",
+    description: "Constantemente aprimorando nossos serviços",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
     titleClassName: "justify-center",
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title: "Entusiastas da tecnologia com paixão pelo desenvolvimento.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -49,8 +49,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
+    title: "Construir um ecosistema automatizado para você",
+    description: "Nossa missão",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
@@ -59,7 +59,7 @@ export const gridItems = [
   },
   {
     id: 6,
-    title: "Do you want to start a project together?",
+    title: "Você quer iniciar um projeto conosco?",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -72,34 +72,41 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    title: "Cloud Computing: Infraestrutura Escalável e Segura",
+    des: "Potencialize sua empresa com soluções em nuvem de alto desempenho. Oferecemos infraestrutura escalável, segura e de alta disponibilidade para suas aplicações e dados, garantindo confiabilidade e eficiência para o seu negócio.",
     img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    iconLists: ["/cloud-computing-icon.svg"], // Ícone de Cloud Computing
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    title: "Automação de Processos com N8N",
+    des: "Simplifique e automatize fluxos de trabalho com o n8n. Conecte sistemas, integre APIs e crie automações personalizadas para otimizar tarefas repetitivas e aumentar a produtividade da sua equipe.",
     img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    iconLists: ["/n8n.svg"],
     link: "/ui.yoom.com",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    title: "Gestão de Dados Inteligente com NocoDB",
+    des: "Transforme bancos de dados em poderosas planilhas colaborativas com o NocoDB. Gerencie, visualize e compartilhe seus dados de forma intuitiva, sem complicações técnicas.",
     img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    iconLists: ["/nocodb.svg"], // Ícone de NocoDB
     link: "/ui.aiimg.com",
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    title: "Banco de Dados PostgreSQL: Segurança e Performance",
+    des: "Estruture seus dados com um dos bancos de dados mais robustos do mercado. O PostgreSQL oferece alta escalabilidade, confiabilidade e segurança para aplicações de qualquer porte.",
     img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    iconLists: ["/postgres.svg"], // Ícone de PostgreSQL
+    link: "/ui.apple.com",
+  },
+  {
+    id: 5,
+    title: "Máquinas Virtuais Sob Medida para Seu Negócio",
+    des: "Hospede suas aplicações em ambientes virtuais otimizados. Oferecemos VMs configuradas para atender às suas necessidades, garantindo flexibilidade, segurança e alto desempenho.",
+    img: "/p5.svg", // Atualizei para um nome de imagem diferente para este projeto
+    iconLists: ["/vm-icon.svg"], // Ícone de Máquina Virtual
     link: "/ui.apple.com",
   },
 ];
